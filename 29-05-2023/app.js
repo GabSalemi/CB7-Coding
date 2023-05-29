b1121174690581c4ff96029fcae02bf343ace4ec
GabSalemi <128749450+GabSalemi@users.noreply.github.com>
2023-05-29 18:03:50 +0200

    Add files via upload

---
+++ 29-05-2023/app.js
@@ -0,0 +1,12 @@
+
+
+const navItems = document.querySelectorAll(".nav__item");
+
+navItems.forEach((element) => {
+  element.addEventListener("click", () => {
+    navItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+    element.classList.add("active");
+  });
+});
